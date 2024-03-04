21772acaf6ada2918041152a24e85011ceedfc27
willch <williamchenthu@gmail.com>
2024-03-03 19:48:28 -0800

    add framework of diffusion

---
+++ diffusion/diffusion_model/scripts/trainer.py
@@ -0,0 +1,10 @@
+from diffusion_model.utils import test as utils_test
+
+def main():
+    print('###########################')
+    utils_test.hello_world()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ diffusion/setup.py
@@ -0,0 +1,8 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='diffusion_model',
+    version='0.1.0',
+    packages=['diffusion_model'],
+)
